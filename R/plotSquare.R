6d4a06d2189a7bcd441a198ccd104301846010f5
Linbo Wang <lbwang@uw.edu>
2015-07-20 22:53:31 -0700

    plotsquarered

---
+++ R/plotSquare.R
@@ -0,0 +1,30 @@
+       
+    #' Plot a circle of radius r.
+    #'
+    #' Normally, we would now describe what
+    #' the function is supposed to do
+    #'
+    #' @param r The radius of the circle to
+    #'   be plotted
+    #' 
+    #' @param r2 haha    
+    #'
+    #' @return None The function is called
+    #'   solely for its side effect
+    #' @examples
+    #' plotCircle(10)
+    #'
+    #' @seealso The general
+    #'   \code{\link[graphics]{plot}} function
+    #'
+    #' @export
+    #' 
+    plotCircleRed <- function(r=1){
+        myAngles <- seq(from=0, to=2*pi,length.out=200)
+        x <- cos(myAngles)
+        y <- sin(myAngles)
+        plot(r*x, r*y, type="l", main=paste0("A Circle of Radius ", r),col="red")
+    }
+    #
+    #
+    #
