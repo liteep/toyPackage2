e94898f4d7349f4518e274eb61f6656ca7c6ff2f
Linbo Wang <lbwang@uw.edu>
2015-07-20 22:19:06 -0700

    Rstudio helps

---
+++ R/plotCircle.R
@@ -0,0 +1,27 @@
+       
+    #' Plot a circle of radius r.
+    #'
+    #' Normally, we would now describe what
+    #' the function is supposed to do
+    #'
+    #' @param r The radius of the circle to
+    #'   be plotted
+    #' 
+    #' @param r2 haha    
+    #'
+    #' @return None The function is called
+    #'   solely for its side effect
+    #' @examples
+    #' plotCircle(10)
+    #'
+    #' @seealso The general
+    #'   \code{\link[graphics]{plot}} function
+    #'
+    #' @export
+    #' 
+    plotCircle <- function(r=1){
+        myAngles <- seq(from=0, to=2*pi,length.out=200)
+        x <- cos(myAngles)
+        y <- sin(myAngles)
+        plot(r*x, r*y, type="l", main=paste0("A Circle of Radius ", r))
+    }
---
+++ R/toyPackage2.R
@@ -0,0 +1,11 @@
+    
+    #' A toy package
+    #'
+    #' This does nothing. I mean nothing.
+    #' What're you looking at?
+    #'
+    #' We can put more verbiage here if we want.
+    #'
+    #' @docType package
+    #' @name toyPackage
+    NULL 
